707f4d38bc4ae0938d2b60693ae6b76d9c8c4d5f
Brandon Silvibarr <brandon.silvi@gmail.com>
2025-10-22 19:09:43 -0700

    added more content to the script with in class lecture material.

--- script.js
+++ script.js
@@ -6,3 +6,87 @@ console.log(false == 0);     // true
 console.log(5 > 2 && 6 > 3); // true
 console.log(false == false ? "equal" : "not equal"); // equal
 console.log(true || false);  // true
+console.log("********************");
+
+// NaN
+console.log("********************");
+console.log("NaNs");
+console.log(NaN);
+
+// null
+console.log("********************");
+console.log("Nulls");
+console.log(null);
+
+// undefined
+console.log("********************");
+console.log("Undefined");
+let myNumber;
+console.log(myNumber); // example of an undefined variable
+
+// number
+console.log("********************");
+console.log("Numbers");
+console.log(15); // example of a whole number
+console.log(2.22); // example of a floating-point number
+console.log(5e5); // example of scientific notation
+
+// boolean
+console.log("********************");
+console.log("Booleans");
+console.log(true);  // example of a boolean value
+console.log(false); // example of a boolean value
+
+// string
+console.log("********************");
+console.log("Strings");
+console.log("Hello, World 1!"); // example of a string with double quotes
+console.log('Hello, World 2!'); // example of a string with single quotes
+console.log(`Hello, World 3!`); // example of a string with backticks
+
+// undefined
+console.log("********************");
+console.log("Undefined");
+let myNumber1;
+console.log(myNumber1); // example of an undefined variable
+
+// special characters when manipulating strings
+console.log("********************");
+console.log("Special Characters in Strings");
+console.log("\nHello"); // \n - New Line
+console.log("\tHello"); // \t -tab space
+///////////////////////////////////////////////////////////
+
+// Operators
+console.log("********************");
+console.log("Operators");
+console.log("********************");
+
+
+// Add
+console.log("********************");
+console.log("Add");
+console.log(5+2);  // adding whole numbers
+console.log(10.5+8.754); // adding floating-points
+console.log("Hello" + "World" + "!"); // concatenating strings
+
+// Subtract
+console.log("********************");
+console.log("Subtract");
+console.log(100-25);  // subtracting whole numbers
+
+
+// Multiply
+console.log("********************");
+console.log("Multiply");
+console.log(91*79);  // multiplying whole numbers
+
+// Divide
+console.log("********************");
+console.log("Divide");
+console.log(12/5);  // dividing whole numbers
+
+// Modulo
+console.log("********************");
+console.log("Modulo");
+console.log(12 % 5);  // remainder of whole number division
